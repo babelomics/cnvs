bb54049dd973098179bd14e998b3d54071e192f1
Susi Gallego Ortega <sgaort@gmail.com>
2015-09-11 12:19:07 +0200

    Modified the colors dain, loss, loh neutral

--- cnvs-web/conf/config.js
+++ cnvs-web/conf/config.js
@@ -2,3 +2,8 @@ CNVS_HOST = "http://localhost:8080/cnvs/rest";
 CELLBASE_VERSION = "v3";
 CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase";
 
+CNVS_TRACK_TYPE_COLORS = {
+    "loss": "red",
+    "gain": "green",
+    "LOH neutral": "blue"
+};
