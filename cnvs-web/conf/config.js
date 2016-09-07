57a912cd8296a8c61a10a67e6dfe69c7ed9c4f29
agaor <sgaort@gmail.com>
2016-09-07 13:25:29 +0200

    Web: colapse all, change in the citoband, add new botton to up, Config: change config

--- cnvs-web/conf/config.js
+++ cnvs-web/conf/config.js
@@ -1,4 +1,5 @@
 CNVS_HOST = "http://test.babelomics.org/cnvs/rest";
+//CNVS_HOST = "http://localhost:8080/cnvs/rest";
 CELLBASE_VERSION = "v3";
 CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase";
 
