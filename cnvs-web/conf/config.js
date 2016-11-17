2fce6c0d9fd2892c23fde38ff67d586fc0354ee5
agaor <sgaort@gmail.com>
2016-11-17 12:53:20 +0100

    Update libraries

--- cnvs-web/conf/config.js
+++ cnvs-web/conf/config.js
@@ -1,7 +1,10 @@
 CNVS_HOST = "http://test.babelomics.org/cnvs/rest";
 //CNVS_HOST = "http://localhost:8080/cnvs/rest";
-CELLBASE_VERSION = "v3";
-CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase";
+//CELLBASE_VERSION = "v3";
+//CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase";
+CELLBASE_VERSION = "v4";
+CELLBASE_HOST = "http://bioinfo.hpc.cam.ac.uk/cellbase";
+CELLBASE_HOST = "http://ws.babelomics.org/cellbase";
 
 CNVS_TRACK_TYPE_COLORS = {
     "loss": "red",
