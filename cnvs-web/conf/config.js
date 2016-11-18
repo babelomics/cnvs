7fb4a2fb110309c2cb9e34471ad9ca53e0e594a2
agaor <sgaort@gmail.com>
2016-11-18 13:32:17 +0100

    Change .war version

--- cnvs-web/conf/config.js
+++ cnvs-web/conf/config.js
@@ -1,7 +1,7 @@
-CNVS_HOST = "http://test.babelomics.org/cnvs/rest";
+CNVS_HOST = "http://test.babelomics.org/cnvs_1.2.4/rest";
 //CNVS_HOST = "http://localhost:8080/cnvs/rest";
-CELLBASE_VERSION = "v3";
-CELLBASE_HOST = "http://bioinfodev.hpc.cam.ac.uk/cellbase";
+CELLBASE_VERSION = "v4";
+CELLBASE_HOST = "http://bioinfo.hpc.cam.ac.uk/cellbase";
 
 CNVS_TRACK_TYPE_COLORS = {
     "loss": "red",
